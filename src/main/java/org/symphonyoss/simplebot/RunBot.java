29bc0997ab5757322fdf0f6a2daf03384404deff
Maurizio Pillitu <maoo@symphony.foundation>
2016-11-21 23:24:42 +0100

    added appassembler maven plugin to simplify CI/CD integration

---
+++ src/main/java/org/symphonyoss/simplebot/RunBot.java
@@ -0,0 +1,76 @@
+/*
+ *
+ * Copyright 2016 The Symphony Software Foundation
+ *
+ * Licensed to The Symphony Software Foundation (SSF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.symphonyoss.simplebot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.symphonyoss.client.SymphonyClient;
+import org.symphonyoss.client.SymphonyClientFactory;
+import org.symphonyoss.client.model.Chat;
+import org.symphonyoss.client.model.SymAuth;
+import org.symphonyoss.client.services.ChatListener;
+import org.symphonyoss.exceptions.*;
+import org.symphonyoss.symphony.agent.model.Message;
+import org.symphonyoss.symphony.agent.model.MessageSubmission;
+import org.symphonyoss.symphony.clients.AuthorizationClient;
+import org.symphonyoss.symphony.clients.model.SymMessage;
+import org.symphonyoss.symphony.clients.model.SymUser;
+import org.symphonyoss.symphony.pod.model.Stream;
+import org.symphonyoss.symphony.pod.model.User;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class RunBot
+{
+    private final static Logger log = LoggerFactory.getLogger(RunBot.class);
+
+    public static void main(String[] args)
+    {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Only one parameter must be passed to RunBot, not "+args.length);
+        }
+        String clazzName = args[0];
+
+        try {
+            Class clazz = Class.forName(clazzName);
+            Object ojb = clazz.newInstance();
+            Method methodToFind = clazz.getMethod("start", (Class<?>[]) null);
+            methodToFind.invoke(ojb);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            //start method not found, ignore
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
+        System.exit(0);
+    }
+}
